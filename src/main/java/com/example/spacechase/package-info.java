4a17acf9e914f64c01aedd5a1c631f40db6916d7
Karl Clifford <2108602@swansea.ac.uk>
2022-11-24 17:55:12 +0000

    package-info.java edited so all group members are included correctly.

--- src/main/java/com/example/spacechase/package-info.java
+++ src/main/java/com/example/spacechase/package-info.java
@@ -1,7 +1,13 @@
 /**
  * This module creates a JavaFX 2D game named Space Chase.
  *
+ * @author Karl Clifford
  * @author Tristan Tsang
+ * @author Ben Thornber
+ * @author Rami Abdulrazzaq
+ * @author Alex Hallsworth
+ * @author Daniel Halsall
+ * @author Jamie Quinn
  * @version 1.0
  */
 
