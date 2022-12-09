4a4adf41012bd94b54404963c01a188e14858bf3
daniel halsall <2120714@swansea.ac.uk>
2022-12-09 14:12:58 +0000

    updated javadoc

--- src/main/java/com/example/spacechase/FlyingAssassin.java
+++ src/main/java/com/example/spacechase/FlyingAssassin.java
@@ -5,7 +5,7 @@ package com.example.spacechase;
  * components of directions and all shared components from character.
  * @author Tristan Tsang
  * @author Daniel Halsall
- * @version 1.0.1
+ * @version 1.0.2
  */
 public class FlyingAssassin extends Character {
     /**
@@ -25,44 +25,38 @@ public class FlyingAssassin extends Character {
     }
 
     /**
-     * sets the initial direction of the flying assassin.
-     * @param direc the id used in the text file for the flying assassin
-     * @return the direction the flying assassin will be
+     * Sets the initial direction of the flying assassin.
+     * @param direc the id used in the text file for the flying assassin.
+     * @return the direction the flying assassin will be.
      */
     public Direction setDirection(char direc) {
-        if (direc == '^') {
-            System.out.println("up");
-            return Direction.UP;
+        switch (direc){
+            case '^':
+                direction = Direction.UP;
+                break;
+            case '>':
+                direction = Direction.RIGHT;
+                break;
+            case '<':
+                direction = Direction.LEFT;
+                break;
+            case '⌄':
+                direction = Direction.DOWN;
+                break;
+
         }
-        if (direc == '<') {
-            System.out.println("left");
-            return Direction.LEFT;
-        }
-        if (direc == '>') {
-            System.out.println("right");
-             return Direction.RIGHT;
-        }
-        return  Direction.DOWN;
+        return direction;
     }
 
     /**
-     * sets the id to the char used in the text file.
+     * Sets the id to the char used in the text file.
      * @param id character representing
-     * the flying assassin in the level text file
+     * the flying assassin in the level text file.
      * @return the character representing
-     * the id of the flying assassin
+     * the id of the flying assassin.
      */
     public char setid(char id) {
-        if (id == '^') {
-            return '^';
-        }
-        if (id == '<') {
-            return '<';
-        }
-        if (id == '>') {
-            return '>';
-        }
-        return '⌄';
+        return id;
     }
 
     /**
@@ -72,29 +66,34 @@ public class FlyingAssassin extends Character {
     @Override
     void move() {
         Tile neighbour = tile.getNeighbourTile(direction);
-        /* If there's no neighbour tile in that direction,
-         change direction to an opposite direction.
-         Otherwise, move to that tile. */
+        /** If there's no neighbour tile in that direction
+         * reverse the direction **/
         if (neighbour == null) {
-            if (direction.equals(Direction.UP)
-                    || direction.equals(Direction.DOWN)) {
-                direction = direction == Direction.UP
-                        ? Direction.DOWN
-                        : Direction.UP;
-                move();
-            }
-            if (direction.equals(Direction.LEFT)
-                    || direction.equals(Direction.RIGHT)) {
-                direction = direction == Direction.LEFT
-                        ? Direction.RIGHT
-                        : Direction.LEFT;
-                move();
+            switch (direction){
+                case UP:
+                    direction = Direction.DOWN;
+                    move();
+                    break;
+                case DOWN:
+                    direction = Direction.UP;
+                    move();
+                    break;
+                case RIGHT:
+                    direction = Direction.LEFT;
+                    move();
+                    break;
+                case LEFT:
+                    direction = Direction.RIGHT;
+                    move();
+                    break;
+
+
             }
         } else {
             Character character = neighbour.getCharacter();
 
-            /* Eliminate the character if there is a character
-             on the tile. */
+            /** Eliminate the character if there is a character
+             on the tile. **/
             if (character != null) {
                 character.remove();
             }
